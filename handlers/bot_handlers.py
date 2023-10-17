d761ecf68263ca282f9f4c3fdd803256d66ed4c9
pyadminrus <zh.vitaliy92@yandex.ru>
2023-10-18 00:26:31 +0300

    Добавлена проверка на зарегистрированного пользователя.

--- handlers/bot_handlers.py
+++ handlers/bot_handlers.py
@@ -1,40 +1,71 @@
 from aiogram import types
-
+import json
 from system.bot_config import dp
+from aiogram.dispatcher import FSMContext
+
+# Загрузка списка идентификаторов авторизованных пользователей из JSON-файла
+with open('setting/authorized_users.json', 'r') as auth_file:
+    authorized_users = json.load(auth_file)
 
 
 @dp.message_handler(commands=['start'])
-async def start(message: types.Message):
+async def start(message: types.Message, state: FSMContext):
     """
     Обработчик команды /start и создание главного меню.
     Этот обработчик вызывается, когда пользователь отправляет команду /start.
-    Он создает клавиатурное меню с опцией "Расчет потерь на линии" и отправляет его пользователю.
+    Он проверяет, имеет ли пользователь доступ к боту на основе его идентификатора (ID). Если пользователь
+    имеет доступ, бот завершает текущее состояние машины состояний (если таковое имеется) и создает клавиатурное меню
+    с опцией "Расчет потерь на линии", которое отправляется пользователю. Если у пользователя нет доступа,
+    он получает уведомление о том, что у него нет доступа к этому боту.
     :param message: Объект, представляющий сообщение от пользователя.
+    :param state: Объект состояния машины состояний (FSMContext), который может использоваться для управления состояниями
+                  в боте (в данном случае, состояние сбрасывается).
     """
-    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    item = types.KeyboardButton("Расчет потерь на линии")
-    markup.add(item)
-    await message.answer("Выберите действие:", reply_markup=markup)
+    user_id = message.from_user.id
+    print(user_id)
+    print("Authorized Users:", authorized_users)
+    print("User ID:", user_id)
+    if int(user_id) in authorized_users:
+        await state.finish()  # Завершаем текущее состояние машины состояний
+        await state.reset_state()  # Сбрасываем все данные машины состояний, до значения по умолчанию
+        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        item = types.KeyboardButton("Расчет потерь на линии")
+        markup.add(item)
+        await message.answer("Выберите действие:", reply_markup=markup)
+    else:
+        await message.answer("У вас нет доступа к этому боту.")
 
 
-# @dp.message_handler(func=lambda message: message.text == "Выйти в главное меню")
-# async def start_key(message: types.Message):
-#     """
-#     Обработчик нажатия на кнопку "Выйти в главное меню".
-#     Этот обработчик вызывается, когда пользователь нажимает кнопку "Выйти в главное меню".
-#     Он также создает клавиатурное меню с опцией "Расчет потерь на линии" и отправляет его пользователю.
-#     :param message: Объект, представляющий сообщение от пользователя.
-#     """
-#     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-#     item = types.KeyboardButton("Расчет потерь на линии")
-#     markup.add(item)
-#     await message.answer("Выберите действие:", reply_markup=markup)
+@dp.message_handler(lambda message: message.text == "Выйти в главное меню")
+async def start_key(message: types.Message, state: FSMContext):
+    """
+    Обработчик нажатия на кнопку "Выйти в главное меню".
+    Этот обработчик вызывается, когда пользователь отправляет сообщение "Выйти в главное меню". Он проверяет,
+    имеет ли пользователь доступ к боту на основе его идентификатора (ID). Если пользователь имеет доступ, бот завершает
+    текущее состояние машины состояний (если таковое имеется) и создает клавиатурное меню с опцией "Расчет потерь на
+    линии", которое отправляется пользователю. Если у пользователя нет доступа, он получает уведомление о том, что у
+    него нет доступа к этому боту.
+    :param message: Объект, представляющий сообщение от пользователя.
+    :param state: Объект состояния машины состояний (FSMContext), который может использоваться для управления
+                  состояниями в боте (в данном случае, состояние сбрасывается).
+    """
+    user_id = message.from_user.id
+    if user_id in authorized_users:
+        await state.finish()  # Завершаем текущее состояние машины состояний
+        await state.reset_state()  # Сбрасываем все данные машины состояний, до значения по умолчанию
+        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        item = types.KeyboardButton("Расчет потерь на линии")
+        markup.add(item)
+        await message.answer("Выберите действие:", reply_markup=markup)
+    else:
+        await message.answer("У вас нет доступа к этому боту.")
 
 
 def register_start_handler():
     """
     Регистрация обработчика команды /start.
-    Эта функция регистрирует обработчик команды /start, чтобы бот мог реагировать на нее.
+    Эта функция регистрирует обработчики для команды /start и нажатия кнопки "Выйти в главное меню", чтобы бот мог
+    реагировать на них и выполнять соответствующие действия.
     """
-    dp.register_message_handler(start)
-    # dp.register_message_handler(start_key)
+    dp.register_message_handler(start)  # Регистрация обработчика для команды /start
+    dp.register_message_handler(start_key)  # Регистрация обработчика для кнопки "Выйти в главное меню"
--- handlers/rashet_poter.py
+++ handlers/rashet_poter.py
@@ -5,6 +5,7 @@ from aiogram import types
 from aiogram.dispatcher import FSMContext
 from aiogram.dispatcher.filters.state import StatesGroup, State
 
+from handlers.bot_handlers import start_key
 from keyboards.user_keyboards import create_inline_keyboard, create_reply_keyboard
 from system.bot_config import dp, bot
 
@@ -12,6 +13,9 @@ from system.bot_config import dp, bot
 with open('setting/provod.json', 'r', encoding='utf-8') as file:
     conductors = json.load(file)
 
+# Load the list of authorized user IDs from a JSON file
+with open('setting/authorized_users.json', 'r') as auth_file:
+    authorized_users = json.load(auth_file)
 
 class MyStates(StatesGroup):
     GET_VOLTAGE = State()
@@ -21,25 +25,64 @@ class MyStates(StatesGroup):
 
 
 @dp.message_handler(lambda message: message.text == "Начать расчет заново")
-async def restart_calculation(message: types.Message):
-    """Обработчик нажатия на кнопку "Начать расчет заново"."""
-    markup = create_reply_keyboard()  # ReplyKeyboardMarkup с кнопками 'Начать расчет заново' и 'Выйти в главное меню'
-    await bot.send_message(message.chat.id, "Расчет начат заново. Введите параметры для расчета потерь на линии:")
-    await bot.send_message(message.chat.id, "Напряжение линии (кВ):", reply_markup=markup)
-    await MyStates.GET_VOLTAGE.set()
-
+async def restart_calculation(message: types.Message, state: FSMContext):
+    """
+    Обработчик нажатия на кнопку "Начать расчет заново".
+    Этот обработчик вызывается, когда пользователь отправляет сообщение "Начать расчет заново". Он проверяет,
+    имеет ли пользователь доступ к боту на основе его идентификатора (ID). Если пользователь имеет доступ, бот завершает
+    текущее состояние машины состояний (если таковое имеется) и сбрасывает все данные машины состояний до значений
+    по умолчанию. Затем он создает клавиатурное меню с кнопками "Начать расчет заново" и "Выйти в главное меню" и
+    отправляет сообщения с инструкциями для пользователя. Наконец, устанавливает состояние машины состояний на GET_VOLTAGE,
+    чтобы начать новый расчет потерь на линии.
+    :param message: Объект, представляющий сообщение от пользователя.
+    :param state: Объект состояния машины состояний (FSMContext), который может использоваться для управления состояниями
+                  в боте (в данном случае, состояние сбрасывается).
+    """
+    user_id = message.from_user.id
+    # Проверка доступа пользователя
+    if user_id in authorized_users:
+        await state.finish()  # Завершаем текущее состояние машины состояний
+        await state.reset_state()  # Сбрасываем все данные машины состояний, до значения по умолчанию
+        markup = create_reply_keyboard()  # ReplyKeyboardMarkup с кнопками 'Начать расчет заново' и 'Выйти в главное меню'
+        await bot.send_message(message.chat.id, "Расчет начат заново. Введите параметры для расчета потерь на линии:")
+        await bot.send_message(message.chat.id, "Напряжение линии (кВ):", reply_markup=markup)
+        # Устанавливаем состояние машины состояний на GET_VOLTAGE
+        await MyStates.GET_VOLTAGE.set()
+    else:
+        await message.answer("У вас нет доступа к этому боту.")
 
 @dp.message_handler(lambda message: message.text == "Расчет потерь на линии")
 async def calculate_loss(message: types.Message):
-    markup = create_reply_keyboard()  # ReplyKeyboardMarkup с кнопками 'Начать расчет заново' и 'Выйти в главное меню'
-    await bot.send_message(message.chat.id, "Введите параметры для расчета потерь на линии:")
-    await bot.send_message(message.chat.id, "Напряжение линии (кВ):", reply_markup=markup)
-    await MyStates.GET_VOLTAGE.set()
+    """
+    Обработчик нажатия на кнопку "Расчет потерь на линии".
+    Этот обработчик вызывается, когда пользователь отправляет сообщение "Расчет потерь на линии". Он проверяет,
+    имеет ли пользователь доступ к боту на основе его идентификатора (ID). Если пользователь имеет доступ, бот создает
+    клавиатурное меню с кнопками "Начать расчет заново" и "Выйти в главное меню" и отправляет сообщения с инструкциями
+    для пользователя. Затем бот устанавливает состояние машины состояний на GET_VOLTAGE, чтобы начать процесс
+    ввода параметров для расчета потерь на линии.
+    :param message: Объект, представляющий сообщение от пользователя.
+    """
+    user_id = message.from_user.id
+    # Проверка доступа пользователя
+    if user_id in authorized_users:
+        markup = create_reply_keyboard()  # ReplyKeyboardMarkup с кнопками 'Начать расчет заново' и 'Выйти в главное меню'
+        await bot.send_message(message.chat.id, "Введите параметры для расчета потерь на линии:")
+        await bot.send_message(message.chat.id, "Напряжение линии (кВ):", reply_markup=markup)
+        # Устанавливаем состояние машины состояний на GET_VOLTAGE
+        await MyStates.GET_VOLTAGE.set()
+    else:
+        await message.answer("У вас нет доступа к этому боту.")
 
 
 @dp.message_handler(state=MyStates.GET_VOLTAGE)
 async def get_voltage(message: types.Message, state: FSMContext):
-    """Функция для получения напряжения линии."""
+    """
+    Обработчик для получения значения напряжения линии.
+    Эта функция ожидает получить значение напряжения линии от пользователя. Если пользователь введет корректное число,
+    она сохранит это значение в данных состояния машины состояний и перейдет к запросу длины линии (состояние GET_LENGTH).
+    :param message: Объект, представляющий сообщение от пользователя.
+    :param state: Объект состояния машины состояний (FSMContext), который используется для управления состояниями в боте.
+    """
     try:
         voltage = float(message.text)
         await state.update_data(voltage=voltage)
@@ -48,34 +91,72 @@ async def get_voltage(message: types.Message, state: FSMContext):
     except ValueError:
         if message.text == "Начать расчет заново":
             # Если пользователь нажал "Начать расчет заново", начнем расчет заново
-            await restart_calculation(message)
-        # elif message.text == "Выйти в главное меню":
-        #     # Если пользователь нажал "Выйти в главное меню", возвращаемся в главное меню
-        #     await start(message)
+            await restart_calculation(message, state)
+        elif message.text == "Выйти в главное меню":
+            # Если пользователь нажал "Выйти в главное меню", возвращаемся в главное меню
+            await start_key(message, state)
         else:
-            await bot.send_message(message.chat.id, "Введите корректное значение напряжения (число):")
+            await bot.send_message(message.chat.id, "Введите корректное значение (число):")
 
 
 @dp.message_handler(state=MyStates.GET_LENGTH)
 async def get_length(message: types.Message, state: FSMContext):
-    """Функция для получения длины линии."""
-    length = float(message.text)
-    await state.update_data(length=length)
-    await bot.send_message(message.chat.id, "Мощность нагрузки (кВт):")
-    await MyStates.GET_LOAD_POWER.set()
+    """
+    Обработчик для получения значения длины линии.
+    Эта функция ожидает получить значение длины линии от пользователя. Если пользователь введет корректное число,
+    она сохранит это значение в данных состояния машины состояний и перейдет к запросу мощности нагрузки (состояние GET_LOAD_POWER).
+    :param message: Объект, представляющий сообщение от пользователя.
+    :param state: Объект состояния машины состояний (FSMContext), который используется для управления состояниями в боте.
+    """
+    try:
+        length = float(message.text)
+        await state.update_data(length=length)
+        await bot.send_message(message.chat.id, "Мощность нагрузки (кВт):")
+        await MyStates.GET_LOAD_POWER.set()
+    except ValueError:
+        if message.text == "Начать расчет заново":
+            await restart_calculation(message, state)
+        elif message.text == "Выйти в главное меню":
+            # Если пользователь нажал "Выйти в главное меню", возвращаемся в главное меню
+            await start_key(message, state)
+        else:
+            await bot.send_message(message.chat.id, "Введите корректное значение (число):")
 
 
 @dp.message_handler(state=MyStates.GET_LOAD_POWER)
 async def get_load_power(message: types.Message, state: FSMContext):
-    load_power = float(message.text)
-    await state.update_data(load_power=load_power)
-    await bot.send_message(message.chat.id, "Введите значение косинуса фи (от 0 до 1 включительно):")
-    await MyStates.GET_POWER_FACTOR.set()  # Set the next state
+    """
+    Обработчик для получения значения мощности нагрузки.
+    Эта функция ожидает получить значение мощности нагрузки от пользователя. Если пользователь введет корректное число,
+    она сохранит это значение в данных состояния машины состояний и перейдет к запросу косинуса фи (состояние GET_POWER_FACTOR).
+    :param message: Объект, представляющий сообщение от пользователя.
+    :param state: Объект состояния машины состояний (FSMContext), который используется для управления состояниями в боте.
+    """
+    try:
+        load_power = float(message.text)
+        await state.update_data(load_power=load_power)
+        await bot.send_message(message.chat.id, "Введите значение косинуса фи (от 0 до 1 включительно):")
+        await MyStates.GET_POWER_FACTOR.set()  # Set the next state
+    except ValueError:
+        if message.text == "Начать расчет заново":
+            await restart_calculation(message, state)
+        elif message.text == "Выйти в главное меню":
+            # Если пользователь нажал "Выйти в главное меню", возвращаемся в главное меню
+            await start_key(message, state)
+        else:
+            await bot.send_message(message.chat.id, "Введите корректное значение (число):")
 
 
 @dp.message_handler(state=MyStates.GET_POWER_FACTOR)
 async def get_power_factor(message: types.Message, state: FSMContext):
-    """Функция для получения значения косинуса фи."""
+    """
+    Обработчик для получения значения косинуса фи.
+    Эта функция ожидает получить значение косинуса фи от пользователя. Если пользователь введет корректное число в
+    диапазоне от 0 до 1 включительно, она сохранит это значение в данных состояния машины состояний и предоставит
+    пользователю клавиатуру с вариантами проводников (create_inline_keyboard(conductors)) для выбора.
+    :param message: Объект, представляющий сообщение от пользователя.
+    :param state: Объект состояния машины состояний (FSMContext), который используется для управления состояниями в боте.
+    """
     try:
         power_factor = float(message.text)
         if 0 <= power_factor <= 1:
@@ -87,20 +168,40 @@ async def get_power_factor(message: types.Message, state: FSMContext):
             await bot.send_message(message.chat.id, "Значение косинуса фи должно быть от 0 до 1 включительно.")
             await MyStates.GET_POWER_FACTOR.set()
     except ValueError:
-        await bot.send_message(message.chat.id, "Введите корректное значение косинуса фи (число от 0 до 1):")
-        await bot.get_send_progress_handler(message.chat.id)(MyStates.GET_POWER_FACTOR)
+        if message.text == "Начать расчет заново":
+            await restart_calculation(message, state)
+        elif message.text == "Выйти в главное меню":
+            # Если пользователь нажал "Выйти в главное меню", возвращаемся в главное меню
+            await start_key(message, state)
+        else:
+            await bot.send_message(message.chat.id, "Введите корректное значение косинуса фи (число от 0 до 1):")
 
 
 @dp.callback_query_handler(lambda call: call.data in conductors.keys(), state=MyStates.GET_POWER_FACTOR)
 async def choose_conductor(call, state: FSMContext):
+    """
+    Обработчик для выбора проводника из вариантов.
+    Эта функция вызывается, когда пользователь выбирает проводник из предоставленных вариантов (представленных в
+    словаре conductors). Она обновляет данные пользователя, добавляя выбранный проводник в данные, а затем переходит
+    к расчету потерь.
+    :param call: Объект, представляющий callback-запрос от пользователя.
+    :param state: Объект состояния машины состояний (FSMContext), который используется для управления состояниями в боте.
+    """
     user_data = await state.get_data()
-    user_data["conductor"] = call.data  # Update the 'conductor' in the user data
+    user_data["conductor"] = call.data  # Обновите «проводник» в данных пользователя.
     await state.update_data(**user_data)
-    # At this point, 'conductor' is updated in the state, and you can proceed with the calculation.
+    # На этом этапе состояние «проводника» обновляется, и можно приступать к расчету.
     await calculate_losses(call.message, state)
 
 
 async def calculate_losses(message: types.Message, state: FSMContext):
+    """
+    Функция для расчета потерь на линии и отправки результата пользователю.
+    Эта функция принимает данные из состояния машины состояний и выполняет расчет потерь на линии. Затем она отправляет
+    результат расчета пользователю в виде сообщения.
+    :param message: Объект, представляющий сообщение от пользователя.
+    :param state: Объект состояния машины состояний (FSMContext), который содержит данные для расчета потерь.
+    """
     data = await state.get_data()
     voltage = data["voltage"]
     length = data["length"]
@@ -109,9 +210,7 @@ async def calculate_losses(message: types.Message, state: FSMContext):
     power_factor = data["power_factor"]
     resistance, reactance, load_tok = conductors[conductor]
     # Расчет потерь на линии
-    result = (10 ** 8) * (
-                resistance * power_factor + reactance * math.sin(math.acos(power_factor)) * load_power * length) / (
-                         ((voltage * 1000) ** 2) * power_factor)
+    result = load_power*(power_factor*resistance+reactance*((1-power_factor**2)**0.5))*reactance*length/(10*voltage**2)
     load_tok1 = (load_power) / (voltage * power_factor * math.sqrt(3))
     result_message = ("Результат расчета потерь на линии:\n\n"
                       f"Напряжение линии:<u> {voltage:.2f} кВ</u>;\n"
